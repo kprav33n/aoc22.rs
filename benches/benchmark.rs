a458b5b512573d2a017ed9f88ff7a25a0072c8e7
Praveen Kumar <praveen+git@kumar.in>
2022-12-01 22:23:37 -0800

    day02: Add benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,16 +1,25 @@
-use aoc22::day01::{max3_total_calories, max_total_calories};
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs;
 
 pub fn bench_day01(c: &mut Criterion) {
     let input = fs::read_to_string("input/day01.txt").unwrap();
     c.bench_function("max_total_calories", |b| {
-        b.iter(|| max_total_calories(&input))
+        b.iter(|| aoc22::day01::max_total_calories(&input))
     });
     c.bench_function("max3_total_calories (BinHeap)", |b| {
-        b.iter(|| max3_total_calories(&input))
+        b.iter(|| aoc22::day01::max3_total_calories(&input))
     });
 }
 
-criterion_group!(benches, bench_day01);
+pub fn bench_day02(c: &mut Criterion) {
+    let input = fs::read_to_string("input/day02.txt").unwrap();
+    c.bench_function("total_score_p1", |b| {
+        b.iter(|| aoc22::day02::total_score_p1(&input))
+    });
+    c.bench_function("total_score_p2", |b| {
+        b.iter(|| aoc22::day02::total_score_p2(&input))
+    });
+}
+
+criterion_group!(benches, bench_day01, bench_day02);
 criterion_main!(benches);
