77174f02a3f59a3df8f7758023047334b4c95732
Praveen Kumar <praveen+git@kumar.in>
2022-12-03 03:45:30 -0800

    day03: Refactor priority calculation

--- src/day03.rs
+++ src/day03.rs
@@ -1,21 +1,25 @@
 use std::collections::HashSet;
 
+fn priority(ch: char) -> i64 {
+    let v = (ch as i64) - 96;
+    if v > 0 {
+        v
+    } else {
+        v + 58
+    }
+}
+
 fn parse_line(s: &str) -> i64 {
     let (first, last) = s.split_at(s.len() / 2);
     let s1 = first.chars().collect::<HashSet<_>>();
     let s2 = &last.chars().collect::<HashSet<_>>();
     let mut ixn = s1.intersection(s2);
     let ch = ixn.next().unwrap();
-    let p = (*ch as i64) - 96;
-    if p > 0 {
-        p
-    } else {
-        p + 58
-    }
+    priority(*ch)
 }
 
 pub fn sum_priorities_p1(s: &str) -> i64 {
-    s.trim().split_terminator("\n").map(parse_line).sum()
+    s.trim().split_terminator('\n').map(parse_line).sum()
 }
 
 pub fn sum_priorities_p2(s: &str) -> i64 {
@@ -29,8 +33,7 @@ pub fn sum_priorities_p2(s: &str) -> i64 {
         s0.retain(|e| s1.contains(e));
         s0.retain(|e| s2.contains(e));
         let ch = s0.iter().next().unwrap();
-        let p = (*ch as i64) - 96;
-        sum += if p > 0 { p } else { p + 58 }
+        sum += priority(*ch);
     }
     sum
 }
