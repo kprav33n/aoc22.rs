43abd74cc557cd90c192fde9776bc3fe2e8ec772
Praveen Kumar <praveen+git@kumar.in>
2022-12-05 00:31:24 -0800

    day05: Refactor stack parsing

--- src/day05.rs
+++ src/day05.rs
@@ -24,6 +24,27 @@ fn parse_step(s: &str) -> (usize, usize, usize) {
     )
 }
 
+// Parse stack representation and return a vector of stacks.
+fn parse_stacks(s: &str) -> Vec<Vec<char>> {
+    let mut stack_lines: Vec<&str> = s.split('\n').collect();
+    let label_line = stack_lines.pop().unwrap();
+    let num_stacks = (label_line.len() + 2) / 4;
+    let stack_rows: Vec<Vec<Option<char>>> = stack_lines
+        .iter()
+        .rev()
+        .map(|s| parse_stack_row(num_stacks, s))
+        .collect();
+    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); num_stacks];
+    for row in stack_rows {
+        for (i, column) in row.iter().enumerate() {
+            if let Some(x) = column {
+                stacks[i].push(*x);
+            }
+        }
+    }
+    stacks
+}
+
 // An Executor can execute the given steps on the given stacks.
 type Executor = fn(&Vec<(usize, usize, usize)>, &mut [Vec<char>]);
 
@@ -48,24 +69,11 @@ fn execute_steps_p2(steps: &Vec<(usize, usize, usize)>, stacks: &mut [Vec<char>]
 
 // Parse stacks, steps and compute top of the stack after executing steps.
 pub fn top_of_stack(s: &str, executor: Executor) -> String {
-    let v: Vec<&str> = s.split("\n\n").collect();
-    let mut pos: Vec<&str> = v[0].split('\n').collect();
-    let label_row = pos.pop().unwrap();
-    let num_stacks = (label_row.len() + 2) / 4;
-    let stack_rows: Vec<Vec<Option<char>>> = pos
-        .iter()
-        .rev()
-        .map(|s| parse_stack_row(num_stacks, s))
-        .collect();
-    let mut stacks: Vec<Vec<char>> = vec![Vec::new(); num_stacks];
-    for row in stack_rows {
-        for (i, column) in row.iter().enumerate() {
-            if let Some(x) = column {
-                stacks[i].push(*x);
-            }
-        }
-    }
-    let steps = v[1].trim().split('\n').map(parse_step).collect();
+    let Some((stacks_str, steps_str)) = s.split_once("\n\n") else {
+        unreachable!();
+    };
+    let mut stacks: Vec<Vec<char>> = parse_stacks(stacks_str);
+    let steps = steps_str.trim().split('\n').map(parse_step).collect();
     executor(&steps, &mut stacks);
 
     let mut tos: Vec<char> = Vec::new();
